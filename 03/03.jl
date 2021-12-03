2284fbd12f511b5f81e1b924cdac017c2b0093c0
Chris Jungerius <cjungerius@gmail.com>
2021-12-03 10:14:17 +0100

    finished day 3

--- 03/03.jl
+++ 03/03.jl
@@ -6,7 +6,6 @@ function fuelcodes(input)
         code[i,:] = parse.(Int,input[i])
     end
 
-
     epsilon=""
     theta=""
     for i in 1:length(code[1,:])
@@ -15,28 +14,38 @@ function fuelcodes(input)
         epsilon *= string(Int(value))
         theta *= string(Int(!value))
     end
-(parse(Int,theta,base=2), parse(Int,epsilon,base=2))
+(parse(Int,theta,base=2)*parse(Int,epsilon,base=2))
 end
 
 function lifesupportcodes(input)
-
-
-    o2 = codefilter(input, epsilon)
-    co2 = codefilter(input, theta)
-    (o2, co2)
-end
-
-function codefilter(input,template)
-    template = string(template)
-    for i in length(template)
-        if length(input) == 1
-            return input[1]
-        else
-            filter!(x->x[i]==template[i],input)
+    o2codes = copy(input)
+    o2 = ""
+    for i in 1:length(o2codes[1])
+        value = Int(count(x->x[i]=='1',o2codes) >= length(o2codes)/2)
+        filter!(x->parse(Int,x[i])==value,o2codes)
+        if length(o2codes)==1
+            o2 = o2codes[1]
+            break
         end
     end
+
+    co2codes = copy(input)
+    co2 = ""
+    for i in 1:length(co2codes[1])
+        value = Int(count(x->x[i]=='1',co2codes) < length(co2codes)/2)
+        filter!(x->parse(Int,x[i])==value,co2codes)
+        if length(co2codes)==1
+            co2 = co2codes[1]
+            break
+        end
+    end
+   
+    (parse(Int,o2,base=2)*parse(Int,co2,base=2))
 end
+
+
 
 input = readlines("input.txt")
 fuelcodes(input)
+lifesupportcodes(input)
 
