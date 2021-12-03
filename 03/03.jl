375fc16543485231d2c5c50016dee2edd7bc72d4
Chris Jungerius <cjungerius@gmail.com>
2021-12-03 12:17:43 +0100

    cleaner solution to 3.1

--- 03/03.jl
+++ 03/03.jl
@@ -1,23 +1,17 @@
 
-function fuelcodes(input)
-    input = split.(input,"")
-    code = zeros(Int,length(input),length(input[1]))
-    for i in 1:length(input)
-        code[i,:] = parse.(Int,input[i])
-    end
-
-    epsilon=""
-    theta=""
-    for i in 1:length(code[1,:])
-        numbers = code[:,i]
-        value = sum(numbers) > length(code[:,1])/2
+function fuelcodes2(input)
+    epsilon = ""
+    theta = ""
+    for i in 1:length(input[1])
+        value = count(x->x[i]=='1',input) > length(input)/2
         epsilon *= string(Int(value))
         theta *= string(Int(!value))
     end
-(parse(Int,theta,base=2)*parse(Int,epsilon,base=2))
+parse(Int,epsilon,base=2)*parse(Int,theta,base=2)
 end
 
 function lifesupportcodes(input)
+    
     o2codes = copy(input)
     o2 = ""
     for i in 1:length(o2codes[1])
@@ -40,7 +34,7 @@ function lifesupportcodes(input)
         end
     end
    
-    (parse(Int,o2,base=2)*parse(Int,co2,base=2))
+    parse(Int,o2,base=2)*parse(Int,co2,base=2)
 end
 
 
