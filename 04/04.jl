a01c26d0aaa9898f59fa12e21139f0ff490b6674
Chris Jungerius <cjungerius@gmail.com>
2021-12-04 14:40:53 +0100

    refactored 04 to give array of all winning scores in order

--- 04/04.jl
+++ 04/04.jl
@@ -1,13 +1,12 @@
 using Base: Bool
 
-
-
 function bingo(input)
 
     sequence = parse.(Int,split(input[1],","))
 
     card = []
     cards = []
+    winners = []
 
     for i in 3:length(input)
         if input[i] != ""
@@ -20,50 +19,7 @@ function bingo(input)
     end
     push!(cards,card)
     
-    cards = cat([hcat(card...) for card in cards]...,dims=3)
-    called = zeros(Bool,size(cards))
-
-    for val in sequence
-        indices = findall(num->num==val,cards)
-        called[indices] .= true
-
-
-        for i in 1:size(called)[2]
-            rowsums = [sum(called[:,i,j]) for j in 1:size(called)[3]]
-            colsums = [sum(called[i,:,j]) for j in 1:size(called)[3]]
-
-            if any(x-> x==size(called)[2],rowsums)
-                winning = argmax(rowsums)
-                return (val * sum(cards[:,:,winning] .* .!called[:,:,winning]))
-            elseif any(x->x==size(called)[2],colsums)
-                winning = argmax(colsums)
-                return (val * sum(cards[:,:,winning] .* .!called[:,:,winning]))
-            end
-        end
-    end
-    cards
-
-end
-
-function bingolast(input)
-
-    sequence = parse.(Int,split(input[1],","))
-
-    card = []
-    cards = []
-
-    for i in 3:length(input)
-        if input[i] != ""
-            line = parse.(Int,split(input[i]))
-            push!(card,line)
-        else
-            push!(cards,card)
-            card=[]
-        end
-    end
-    push!(cards,card)
-    
-    losers = [1:length(cards)...]
+    waiting = Set([1:length(cards)...])
     cards = cat([hcat(card...) for card in cards]...,dims=3)
     called = zeros(Bool,size(cards))
 
@@ -77,18 +33,22 @@ function bingolast(input)
             colsums = [sum(called[i,:,j]) for j in 1:size(called)[3]]
 
             winning = [findall(x->x==size(called)[2],rowsums)..., findall(x->x==size(called)[2],colsums)...] 
-            if length(losers) != 1
-                filter!(x->!(x in winning), losers)
-            elseif length(losers) == 1 && losers[1] in winning
-                loser = losers[1]
-                return val * sum(cards[:,:,loser] .* .!called[:,:,loser])
+
+            for winner in winning
+                if winner in waiting
+                    pop!(waiting, winner)
+                    push!(winners,val * sum(cards[:,:,winner] .* .!called[:,:,winner]))
+                end
+
             end
         end
     end
-    cards
+    winners
 
 end
 
 input = readlines("input.txt")
-bingo(input)
-bingolast(input)
+
+winorder  = bingo(input)
+partone = winorder[1]
+parttwo = winorder[end]
