fee0fd31b3afbf1e3397ce397708c2b7cb040ac6
Chris Jungerius <cjungerius@gmail.com>
2021-12-06 15:40:07 +0100

    added linalg solution to 06

--- 06/06.jl
+++ 06/06.jl
@@ -19,6 +19,29 @@ function fishcount(input,cycles)
 state
 end
 
+function linalgfish(input,cycles)
+
+    input = parse.(Int,split(input,","))
+    state = zeros(Int,9)
+    
+    #initialize state
+    for i in 1:9
+        state[i] = count(x->x==i-1,input)
+    end
+
+    #transition matrix
+    A = zeros(Int, (9,9))
+    for i in 1:8
+        A[i+1,i] = 1
+    end
+    A[1,7] = 1
+    A[1,9] = 1
+
+    #calc
+    state'A^cycles
+end   
+
+
 input = readline("input.txt")
 partone = fishcount(input,80)
 parttwo = fishcount(input,256)
