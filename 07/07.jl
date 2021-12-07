61d0db61c13bd34edfc4868bdd78bf88774c3f68
Chris Jungerius <cjungerius@gmail.com>
2021-12-07 11:11:05 +0100

    solved day 7 part two

--- 07/07.jl
+++ 07/07.jl
@@ -1,8 +1,29 @@
 using Statistics
 
+
+
+function partone(input)
+    #value where sum of absolute distance to all points is minimal is just the median
+    xtilde = median(input)
+    Int(sum(abs.(input.-xtilde)))
+end
+
+
+function parttwo(input)
+    #since triangular error term has a quadratic, optimum is probably either the mean or close to the mean: since we're working with integers, let's look in the neighbourhood
+    triangular = x->x*(x+1)/2
+    target = round(mean(input))
+    results = []
+    for i in target-5:target+5
+        result = triangular.(abs.(input.-i))
+        push!(results,result)
+    end
+    Int(minimum(sum.(results)))
+end
+
 input = readline("input.txt")
 input = split(input,",")
 input = parse.(Int,input)
-xtilde = median(input)
 
-partone = sum(abs.(input.-xtilde))
+one = partone(input)
+two = parttwo(input)
