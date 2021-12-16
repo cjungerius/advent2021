523616c2acb4100c9b07c536ee984303796bc31b
Chris Jungerius <cjungerius@gmail.com>
2021-12-16 23:01:03 +0100

    refactored 16

--- 16/16.jl
+++ 16/16.jl
@@ -3,7 +3,7 @@ input = readline("input.txt")
 function preprocess(input)
     b = ""
     for i in input
-        b *= bitstring(parse(Int,i,base=16))[end-3:end]
+        b *= bitstring(parse(Int, i, base=16))[end - 3:end]
     end
     b
 end
@@ -21,50 +21,50 @@ function parsepacket(packet)
     ])
 
     version = packet[1:3]
-    version = parse(Int,version,base=2)
+    version = parse(Int, version, base=2)
     type = packet[4:6]
 
-    if type=="100"
+    if type == "100"
         value = ""
         readlength = 6
         content = packet[7:end]
         position = 1
         while true
-            value *= content[position+1:position+4]
-            if content[position]=='0'
+            value *= content[position + 1:position + 4]
+            if content[position] == '0'
                 break
             end
             position += 5
         end
-        readlength += position+4
-        value = parse(Int,value,base=2)
+        readlength += position + 4
+        value = parse(Int, value, base=2)
 
     else
         subvals = []
         lengthtype = packet[7]
         if lengthtype == '0'
             readlength = 22
-            subpacketbitlength = parse(Int,packet[8:22],base=2)
-            content = packet[23:23+subpacketbitlength]
+            subpacketbitlength = parse(Int, packet[8:22], base=2)
+            content = packet[23:23 + subpacketbitlength]
             readlength += subpacketbitlength
             while subpacketbitlength > 0
                 v, subval, l = parsepacket(content)
                 version += v
-                push!(subvals,subval)
-                content = content[l+1:end]
+                push!(subvals, subval)
+                content = content[l + 1:end]
                 subpacketbitlength -= l
             end
     
         elseif lengthtype == '1'
             readlength = 18
-            subpacketnumber = parse(Int,packet[8:18], base=2)
+            subpacketnumber = parse(Int, packet[8:18], base=2)
             content = packet[19:end]
             while subpacketnumber > 0
                 v, subval, l = parsepacket(content)
-                push!(subvals,subval)
+                push!(subvals, subval)
                 version += v
                 readlength += l
-                content = content[l+1:end]
+                content = content[l + 1:end]
                 subpacketnumber -= 1
             end
         end
@@ -74,9 +74,6 @@ function parsepacket(packet)
 
     end
     
-    if value == ""
-        value = "0"
-    end
     (version, value, readlength)
 end
 
