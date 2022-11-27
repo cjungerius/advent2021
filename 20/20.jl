ffd96fb4c764ba57a4be7ed69514ba9bbb069e0d
Chris Jungerius <cjungerius@gmail.com>
2022-11-27 15:42:31 +0100

    solved day 20

--- 20/20.jl
+++ 20/20.jl
@@ -10,27 +10,28 @@ end
 
 function enhance(image, algo, n)
 
-enhancements = 0
-
-
-
 for i in 1:n
-        surround = Int(algo[1] * n % 2)
-        newimage = fill(surround,size(image).+2)
-        newimage[2:end-1,2:end-1] = image
-        image = copy(newimage)
+        oldsurround = Int(algo[1] * (i+1) % 2)
+        newimage = fill(oldsurround,size(image).+4)
+        newimage[3:end-2,3:end-2] = image
+        image = deepcopy(newimage)
 
         for i in 1:size(image)[1]-2, j in 1:size(image)[2]-2
-                key = reshape(newimage[i:i+2,j:j+2],9)
+                key = reshape(transpose(newimage[i:i+2,j:j+2]),9)
                 key = *(string.(key)...)
                 newval = (parse(Int,key,base=2))
                 image[i+1,j+1] = algo[newval+1]
         end
 
-        
+        newsurround = Int(algo[1] * (i) % 2)
+        image[1:end,1] .= newsurround
+        image[1:end,end] .= newsurround
+        image[1,1:end] .= newsurround
+        image[end,1:end] .= newsurround
+
 end
 
 image
 end
 
-result = enhance(image, algo, 2)
+result = enhance(image, algo, 50)
