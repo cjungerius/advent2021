5b24678d00b80975a3d670340c4d9c637205f072
Chris Jungerius <cjungerius@gmail.com>
2022-11-27 17:16:49 +0100

    solved day 21 part 2

--- 21/21.jl
+++ 21/21.jl
@@ -1,3 +1,4 @@
+using Memoize 
 
 function turn(die, player)
 
@@ -7,13 +8,13 @@ function turn(die, player)
         die > 100 ? die -= 100 : die
     end
 
-    player%10 == 0 ? player = 10 : player = player%10
+    player = mod1(player,10)
 
     (die, player)
 end 
 
 
-function diracdice(a,b)
+function deterministic(a,b)
     die = 1
     rolls = 0
     sa = 0
@@ -39,3 +40,24 @@ function diracdice(a,b)
 end
 
 
+
+@memoize function diracdice(a,sa, b, sb, turn)
+    if sa >= 21
+        return 1
+    elseif sb >= 21
+        return 0
+
+    else
+        universes = vec([a+b+c for a in 1:3, b in 1:3, c in 1:3])
+        if turn == 0
+            new_a = mod1.(a.+universes,10)
+            new_sa = sa.+new_a
+
+            return sum([diracdice(x,y,b,sb,1) for (x,y) in zip(new_a,new_sa)])
+        elseif turn == 1
+            new_b = mod1.(b.+universes,10)
+            new_sb = sb.+new_b
+            return sum([diracdice(a,sa,x,y,0) for (x,y) in zip(new_b,new_sb)])
+        end
+    end
+end
